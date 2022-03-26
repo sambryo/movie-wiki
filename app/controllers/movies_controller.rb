d1166373289897e0bf2fc405a5e633f9f5e712ed
Samuel <terefesam@gmail.com>
2022-03-25 19:31:08 -0700

    routest show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all 
+  end
+  def show
+    #fail
+    @movie = Movie.find(params[:id])
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show"
 end
