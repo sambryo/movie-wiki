dd72a5a471426401241c3d914a144ab4a3bedf57
Samuel <terefesam@gmail.com>
2022-03-26 19:57:34 -0700

    create movie and movie_params in private

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,4 +20,16 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new 
   end 
+
+  def create
+      
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end 
+  
+  private 
+    def movie_params
+      params.require(:movie).permit(:title, :description, :total_gross, :released_on, :rating)
+    end 
 end
