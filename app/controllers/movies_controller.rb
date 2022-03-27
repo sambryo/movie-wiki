ab2d7047f0c6fd7c50f1f30498598f90fc35799a
Samuel <terefesam@gmail.com>
2022-03-26 21:21:03 -0700

    Fixed link_to on Delete Problem

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,12 +22,17 @@ class MoviesController < ApplicationController
   end 
 
   def create
-      
     @movie = Movie.new(movie_params)
     @movie.save
     redirect_to @movie
   end 
-  
+
+  def destroy 
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url 
+  end 
+
   private 
     def movie_params
       params.require(:movie).permit(:title, :description, :total_gross, :released_on, :rating)
