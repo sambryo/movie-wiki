6101bd48efe80b6b53512de46c918b4743a843ba
Samuel <terefesam@gmail.com>
2022-03-28 19:02:45 -0700

    custom query

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all 
+    @movies = Movie.not_flop
   end
   def show
     #fail
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  def self.not_flop
+    Movie.where("total_gross > ?",100000000)
+  end 
   def flop? 
     total_gross.blank? || total_gross < 100000000 
   end 
